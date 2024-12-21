5f0eeaf7a2c9e675bc178a88b1d75a9864405ae8
greenteagarlicbread <132750309+greenteagarlicbread@users.noreply.github.com>
2024-12-21 23:09:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bots = [
   {
     token: process.env.TOKEN_1,
     statusMessages: ["🧐 Watching out for spawns!", "💖 Made by Roti with love"],
-    statusTypes: ['idle'],
+    statusTypes: ['online'],
   },
   {
     token: process.env.TOKEN_2,
@@ -12,7 +12,7 @@ const bots = [
   {
     token: process.env.TOKEN_3,
     statusMessages: ["❄️ Merry Christmas SFB!", "🎁 Run /candy daily"],
-    statusTypes: ['dnd'],
+    statusTypes: ['online'],
   },
 ];
 
